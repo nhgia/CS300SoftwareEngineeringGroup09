d88dd034ac5a24369e00b7a8a122b12f82b44f89
Duy Pham <pbduy99@gmail.com>
2020-01-02 14:51:23 +0700

    Google font

--- src/schoodle/lib/MainScreen.dart
+++ src/schoodle/lib/MainScreen.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("Schoodle"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'SCHOODLE',
+            style: GoogleFonts.poppins(
+              textStyle: Theme.of(context).textTheme.display1,
+              fontSize: 40,
+              fontWeight: FontWeight.w800,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+        ),
       ),
-    ),);
+    );
   }
 }
